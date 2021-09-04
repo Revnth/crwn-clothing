a962e5f2678ec005f2fabe2e5625f1c9267f3938
Revanth <revanth123.rp40@gmail.com>
2021-09-05 01:06:17 +0530

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JW3V4SDJyPes6JPuRr4OzMrlIYunqVgzJyz7Z1NbrkgWIWd0o2r0RWbnRL4PDMemcdJfjIFywXUmVG5u0TUvhzh00PGmBQGKP";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
